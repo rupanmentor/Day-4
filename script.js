43b7ba3f7615c56f2a8b58399b67f4bd2d35a6ab
rupanmentor <rupanguvimentor@gmail.com>
2025-09-08 19:16:17 +0530

    normal function completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//Function types example
+//Function types example 
+//Question : print odd numbers in an array [1,2,3,4,5,6,7,8,9,10]
+
+//1. Normal function
+var result = []
+function odd (arr){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]%2!==0){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+console.log(odd([1,2,3,4,5,6,7,8,9,10]));
